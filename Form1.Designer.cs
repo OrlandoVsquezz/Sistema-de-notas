ad0c32222b1139a2f51342c760dfae813b5ec616
Daniel <20250273@ricaldone.edu.sv>
2025-06-12 15:43:31 -0600

    Promedios

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Calculo_de_romedios
+{
+    partial class fmrCalculoPromedios
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmrCalculoPromedios));
+            this.gbRegistroNotas = new System.Windows.Forms.GroupBox();
+            this.lblNotaTres = new System.Windows.Forms.Label();
+            this.lblNotaDos = new System.Windows.Forms.Label();
+            this.lblNotaUno = new System.Windows.Forms.Label();
+            this.gbPromedio = new System.Windows.Forms.GroupBox();
+            this.gbAcciones = new System.Windows.Forms.GroupBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.btmSalir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnAgregarPromedio = new System.Windows.Forms.Button();
+            this.btnNuevoRegistro = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tlpAyuda = new System.Windows.Forms.ToolTip(this.components);
+            this.gbRegistroNotas.SuspendLayout();
+            this.gbPromedio.SuspendLayout();
+            this.gbAcciones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbRegistroNotas
+            // 
+            this.gbRegistroNotas.Controls.Add(this.pictureBox1);
+            this.gbRegistroNotas.Controls.Add(this.lblError);
+            this.gbRegistroNotas.Controls.Add(this.txtNota2);
+            this.gbRegistroNotas.Controls.Add(this.txtNota1);
+            this.gbRegistroNotas.Controls.Add(this.txtNota3);
+            this.gbRegistroNotas.Controls.Add(this.lblNotaTres);
+            this.gbRegistroNotas.Controls.Add(this.lblNotaDos);
+            this.gbRegistroNotas.Controls.Add(this.lblNotaUno);
+            this.gbRegistroNotas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbRegistroNotas.Location = new System.Drawing.Point(51, 68);
+            this.gbRegistroNotas.Name = "gbRegistroNotas";
+            this.gbRegistroNotas.Size = new System.Drawing.Size(750, 387);
+            this.gbRegistroNotas.TabIndex = 0;
+            this.gbRegistroNotas.TabStop = false;
+            this.gbRegistroNotas.Text = "Registro de notas";
+            // 
+            // lblNotaTres
+            // 
+            this.lblNotaTres.AutoSize = true;
+            this.lblNotaTres.Location = new System.Drawing.Point(92, 273);
+            this.lblNotaTres.Name = "lblNotaTres";
+            this.lblNotaTres.Size = new System.Drawing.Size(88, 25);
+            this.lblNotaTres.TabIndex = 2;
+            this.lblNotaTres.Text = "Nota 3:";
+            // 
+            // lblNotaDos
+            // 
+            this.lblNotaDos.AutoSize = true;
+            this.lblNotaDos.Location = new System.Drawing.Point(92, 178);
+            this.lblNotaDos.Name = "lblNotaDos";
+            this.lblNotaDos.Size = new System.Drawing.Size(88, 25);
+            this.lblNotaDos.TabIndex = 1;
+            this.lblNotaDos.Text = "Nota 2:";
+            // 
+            // lblNotaUno
+            // 
+            this.lblNotaUno.AutoSize = true;
+            this.lblNotaUno.Location = new System.Drawing.Point(92, 80);
+            this.lblNotaUno.Name = "lblNotaUno";
+            this.lblNotaUno.Size = new System.Drawing.Size(88, 25);
+            this.lblNotaUno.TabIndex = 0;
+            this.lblNotaUno.Text = "Nota 1:";
+            // 
+            // gbPromedio
+            // 
+            this.gbPromedio.Controls.Add(this.textBox4);
+            this.gbPromedio.Controls.Add(this.label4);
+            this.gbPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbPromedio.Location = new System.Drawing.Point(51, 478);
+            this.gbPromedio.Name = "gbPromedio";
+            this.gbPromedio.Size = new System.Drawing.Size(1040, 167);
+            this.gbPromedio.TabIndex = 1;
+            this.gbPromedio.TabStop = false;
+            this.gbPromedio.Text = "Promedio";
+            // 
+            // gbAcciones
+            // 
+            this.gbAcciones.Controls.Add(this.btmSalir);
+            this.gbAcciones.Controls.Add(this.btnAgregarPromedio);
+            this.gbAcciones.Controls.Add(this.btnNuevoRegistro);
+            this.gbAcciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbAcciones.Location = new System.Drawing.Point(826, 68);
+            this.gbAcciones.Name = "gbAcciones";
+            this.gbAcciones.Size = new System.Drawing.Size(265, 387);
+            this.gbAcciones.TabIndex = 1;
+            this.gbAcciones.TabStop = false;
+            this.gbAcciones.Text = "Acciones";
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(305, 266);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(240, 31);
+            this.txtNota3.TabIndex = 3;
+            this.tlpAyuda.SetToolTip(this.txtNota3, "Ingresa valores entre 0 y 10");
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(305, 73);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(240, 31);
+            this.txtNota1.TabIndex = 4;
+            this.tlpAyuda.SetToolTip(this.txtNota1, "Ingresa valores entre 0 y 10");
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(305, 171);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(240, 31);
+            this.txtNota2.TabIndex = 5;
+            this.tlpAyuda.SetToolTip(this.txtNota2, "Ingresa valores entre 0 y 10");
+            // 
+            // btmSalir
+            // 
+            this.btmSalir.Image = global::Calculo_de_romedios.Properties.Resources.icons8_cerrar_ventana_48;
+            this.btmSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btmSalir.Location = new System.Drawing.Point(6, 266);
+            this.btmSalir.Name = "btmSalir";
+            this.btmSalir.Size = new System.Drawing.Size(252, 106);
+            this.btmSalir.TabIndex = 2;
+            this.btmSalir.Text = "Salir";
+            this.btmSalir.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Promedio Obtenido";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(305, 75);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(240, 31);
+            this.textBox4.TabIndex = 6;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(105, 330);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(64, 25);
+            this.lblError.TabIndex = 6;
+            this.lblError.Text = "Error";
+            // 
+            // btnAgregarPromedio
+            // 
+            this.btnAgregarPromedio.Image = global::Calculo_de_romedios.Properties.Resources.icons8_añadir_usuario_masculino_48;
+            this.btnAgregarPromedio.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAgregarPromedio.Location = new System.Drawing.Point(6, 30);
+            this.btnAgregarPromedio.Name = "btnAgregarPromedio";
+            this.btnAgregarPromedio.Size = new System.Drawing.Size(252, 95);
+            this.btnAgregarPromedio.TabIndex = 1;
+            this.btnAgregarPromedio.Text = "Agregar promedio";
+            this.btnAgregarPromedio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAgregarPromedio.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevoRegistro
+            // 
+            this.btnNuevoRegistro.Image = global::Calculo_de_romedios.Properties.Resources.icons8_escoba_48;
+            this.btnNuevoRegistro.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNuevoRegistro.Location = new System.Drawing.Point(6, 142);
+            this.btnNuevoRegistro.Name = "btnNuevoRegistro";
+            this.btnNuevoRegistro.Size = new System.Drawing.Size(252, 96);
+            this.btnNuevoRegistro.TabIndex = 0;
+            this.btnNuevoRegistro.Text = "Nuevo registro";
+            this.btnNuevoRegistro.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNuevoRegistro.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Calculo_de_romedios.Properties.Resources.icons8_usuario_100;
+            this.pictureBox1.Location = new System.Drawing.Point(598, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tlpAyuda
+            // 
+            this.tlpAyuda.IsBalloon = true;
+            this.tlpAyuda.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.tlpAyuda.ToolTipTitle = "Mensaje de ayuda";
+            // 
+            // fmrCalculoPromedios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 773);
+            this.Controls.Add(this.gbAcciones);
+            this.Controls.Add(this.gbPromedio);
+            this.Controls.Add(this.gbRegistroNotas);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fmrCalculoPromedios";
+            this.Text = "Sistema de notas ITR";
+            this.gbRegistroNotas.ResumeLayout(false);
+            this.gbRegistroNotas.PerformLayout();
+            this.gbPromedio.ResumeLayout(false);
+            this.gbPromedio.PerformLayout();
+            this.gbAcciones.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbRegistroNotas;
+        private System.Windows.Forms.Label lblNotaTres;
+        private System.Windows.Forms.Label lblNotaDos;
+        private System.Windows.Forms.Label lblNotaUno;
+        private System.Windows.Forms.GroupBox gbPromedio;
+        private System.Windows.Forms.GroupBox gbAcciones;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Button btmSalir;
+        private System.Windows.Forms.Button btnAgregarPromedio;
+        private System.Windows.Forms.Button btnNuevoRegistro;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip tlpAyuda;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_de_romedios
+{
+    public partial class fmrCalculoPromedios : Form
+    {
+        public fmrCalculoPromedios()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
